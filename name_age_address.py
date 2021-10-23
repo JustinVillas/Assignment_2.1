06010cb7f253211323225a145a6dc4130cf64e68
JustinVillas <villasjustinlaw@gmail.com>
2021-10-24 00:03:28 +0800

    Initial commit

---
+++ name_age_address.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+address = input("Where do you live? ")
+print("Hi, my name is " + name + ". I am " + age +
+      " years old and I live in " + address + ".")
